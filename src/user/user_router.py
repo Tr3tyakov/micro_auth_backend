147d745e65c869d6810ea122262cc0ccc17785a2
Tretyakov <Imtr3tyakov@gmail.com>
2023-06-09 17:17:16 +0300

    Added user router

---
+++ src/user/user_router.py
@@ -0,0 +1,25 @@
+from fastapi import Depends, APIRouter, HTTPException
+from src.user.user_service import UserService
+
+router = APIRouter()
+
+
+@router.get('/get_all_users', )
+async def get_all_users(service: UserService = Depends()):
+    return await service.get_all_user()
+
+
+@router.get('/get_info/{id}')
+async def get_info(id: int, service: UserService = Depends()):
+    try:
+        return await service.get_user(id=id)
+    except HTTPException as exec:
+        raise exec
+
+
+@router.post('/delete_user/{id}')
+async def delete_user(id: int, service: UserService = Depends()):
+    try:
+        return await service.delete_user(id=id)
+    except HTTPException as exec:
+        raise exec
